ed3a54699225ad61cd73ee665117534c42f085c5
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-04-14 09:16:54 +0300

    Selectors Updated

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,8 @@
 export const getContacts = state => state.contacts;
 export const getFilter = state => state.filter;
 
-// export const getContacts = state => state.contacts.items;
-// export const getFilter = state => state.contacts.filter;
+// export const selectContacts = state => state.contacts.items;
+// export const selectFilter = state => state.filter;
+// export const selectLoading = state => state.contacts.isLoading;
+// export const selectError = state => state.contacts.error;
+ 
